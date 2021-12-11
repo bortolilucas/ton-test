c087aca41d24975f7012ed4eff762dbdaa79d378
Lucas Bortoli <lucasbortolidev@gmail.com>
2021-12-11 01:34:23 -0300

    test: mock useHeaderHeight

--- src/jest/setup.ts
+++ src/jest/setup.ts
@@ -4,3 +4,8 @@ jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+
+jest.mock('@react-navigation/elements', () => ({
+  ...jest.requireActual('@react-navigation/elements'),
+  useHeaderHeight: jest.fn(),
+}));
